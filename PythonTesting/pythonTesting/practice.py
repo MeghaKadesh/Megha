0cabe65c821e9989b4687834bfcfa9da7cdaf0f9
kadeshsg <meghadhaduti@gmail.com>
2025-05-15 22:54:23 +0530

    thried commit

--- PythonTesting/pythonTesting/practice.py
+++ PythonTesting/pythonTesting/practice.py
@@ -5,4 +5,4 @@ driver = webdriver.Chrome()
 driver.get("https://int.zigzag.lk/collections/blazers-jackets")
 driver.find_element(By.XPATH,"//label[@for='Filter-Style size-1'][span]").click()
 driver.find_element(By.ID,"URl")
-
+driver.find_element(By.CSS_SELECTOR,"#url")
