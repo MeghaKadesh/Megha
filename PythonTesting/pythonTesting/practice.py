a2e09a94cbaf47f3a5f3528781e9f9bcc390a6e5
kadeshsg <meghadhaduti@gmail.com>
2025-05-15 22:27:22 +0530

    second commiit

--- PythonTesting/pythonTesting/practice.py
+++ PythonTesting/pythonTesting/practice.py
@@ -4,3 +4,5 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get("https://int.zigzag.lk/collections/blazers-jackets")
 driver.find_element(By.XPATH,"//label[@for='Filter-Style size-1'][span]").click()
+driver.find_element(By.ID,"URl")
+
